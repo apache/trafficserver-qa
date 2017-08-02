3116a2161c5e0be585accfa6fc96ed4391ed7ca6
cclauss <cclauss@bluewin.ch>
2017-08-02 23:19:52 +0200

    import os, tempfile, unittest

--- examples/environment_helpers.py
+++ examples/environment_helpers.py
@@ -18,6 +18,9 @@ which will override the getEnv() method.
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import os
+import tempfile
+import unittest
 import tsqa.test_cases
 
 
